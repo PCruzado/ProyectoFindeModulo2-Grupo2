312cb8c19c5543a69a92fa2d71ec79c08adf68f8
Lucas <lucasarbasualdo@gmail.com>
2025-12-03 03:34:16 -0300

    agregue la logica al boton modificar creando un nuevo componente

--- src/components/Administracion.jsx
+++ src/components/Administracion.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Table, Button } from "react-bootstrap";
 import { DateTime } from "luxon";
 import { useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 const Administracion = () => {
   const [turnos, setTurnos] = useState([]);
@@ -13,37 +13,30 @@ const Administracion = () => {
     if (stored) setTurnos(JSON.parse(stored));
   }, []);
 
- const handleBorrarTurno = (turnoAEliminar) => {
-    
+  const handleBorrarTurno = (turnoAEliminar) => {
     Swal.fire({
-      title: '¿Estás seguro?',
+      title: "¿Estás seguro?",
       text: `Vas a eliminar el turno de ${turnoAEliminar.nombreApellido} para ${turnoAEliminar.nombreMascota}. ¡Esta acción es irreversible!`,
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#DB8307', 
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Sí, ¡Borrar!',
-      cancelButtonText: 'Cancelar'
+      confirmButtonColor: "#DB8307",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Sí, ¡Borrar!",
+      cancelButtonText: "Cancelar",
     }).then((result) => {
-    
       if (result.isConfirmed) {
-        
-        
         const turnosActualizados = turnos.filter(
           (turno) => turno.inicio !== turnoAEliminar.inicio
         );
 
-      
         localStorage.setItem("turnos", JSON.stringify(turnosActualizados));
-        
-       
+
         setTurnos(turnosActualizados);
-        
-      
+
         Swal.fire(
-          '¡Eliminado!',
-          'El turno ha sido eliminado correctamente.',
-          'success'
+          "¡Eliminado!",
+          "El turno ha sido eliminado correctamente.",
+          "success"
         );
       }
     });
@@ -88,8 +81,20 @@ const Administracion = () => {
                 <td>{dt.toLocaleString(DateTime.TIME_SIMPLE)}</td>
                 <td>{turno.motivo}</td>
                 <td className="d-flex gap-2">
-                  <Button variant="primary" size="sm">Modificar</Button>
-                  <Button variant="info" size="sm">Ver</Button>
+                  
+                  <Button
+                    variant="primary"
+                    size="sm"
+                    onClick={() =>
+                      navigate("/modificar-turno", { state: turno })
+                    }
+                  >
+                    Modificar
+                  </Button>
+
+                  <Button variant="info" size="sm">
+                    Ver
+                  </Button>
                   <Button
                     variant="danger"
                     size="sm"
